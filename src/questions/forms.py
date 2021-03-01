69e5eaf62713ab8ea8baed101ff573e50725acc6
rafaellcoellho <rafaellcoellho@gmail.com>
2021-03-01 20:06:44 -0300

    finalmente consegue responder uma pergunta \o/

---
+++ src/questions/forms.py
@@ -0,0 +1,14 @@
+from django.forms import ModelForm, Textarea, HiddenInput
+from .models import Answer
+
+class AnswerForm(ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content', 'question']
+        labels = {
+            'content': 'Answer the question',
+        }
+        widgets = {
+            'content': Textarea(attrs={'rows':3}),
+            'question': HiddenInput(),
+        }
--- src/questions/views.py
+++ src/questions/views.py
@@ -1,13 +1,37 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import CreateView
 
 from .models import Question
+from .forms import AnswerForm
 
 
-class ReceivedQuestionsView(DetailView):
+class ReceivedQuestionsView(CreateView):
     template_name = "question/received_questions.html"
 
     def get(self, request):
         my_user_id = request.user.id
-        unanswered_questions = Question.objects.filter(answer__id__isnull=True, id=my_user_id)
-        return render(request, self.template_name, {'unanswered_questions': unanswered_questions})
+        unanswered_questions_queryset = Question.objects.filter(answer__id__isnull=True, id=my_user_id)
+
+        def process_information(question):
+            return {
+                "form": AnswerForm(initial={"question": question.id}),
+                "question": question
+            }
+
+        infos = map(process_information, unanswered_questions_queryset)
+
+        context = {
+            "infos_unanswered_questions": infos
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            form.save(commit=False)
+
+        context = {
+            "form": form
+        }
+        return render(request, self.template_name, context)
