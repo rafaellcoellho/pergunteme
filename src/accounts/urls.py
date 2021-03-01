379641e6256ff5ef902cea9d5a5bebb073d817ca
rafaellcoellho <rafaellcoellho@gmail.com>
2021-03-01 09:13:51 -0300

    um monte de coisa, não sei mais

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import SignUpView
+from django.contrib.auth.decorators import login_required
+from .views import SignUpView, ExploreView, ProfileView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("explore/", login_required(ExploreView.as_view()), name="explore"),
+    path("<str:user_username>/", login_required(ProfileView.as_view()), name="profile"),
 ]
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,10 +1,29 @@
 from django.urls import reverse_lazy
+from django.shortcuts import render
 from django.views.generic.edit import CreateView
+from django.db.models import Q
 
 from .forms import CustomUserCreationForm
+from .models import CustomUser
 
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+class ExploreView(CreateView):
+    template_name = "account/explore.html"
+
+    def get(self, request):
+        my_user_id = request.user.id
+        all_users = CustomUser.objects.exclude(id=my_user_id).exclude(is_superuser=True)
+        return render(request, self.template_name, {'all_users': all_users})
+
+
+class ProfileView(CreateView):
+    template_name = "account/profile.html"
+
+    def get(self, request, user_username):
+        user = CustomUser.objects.get(username=user_username)
+        return render(request, self.template_name, {'profile': user})
